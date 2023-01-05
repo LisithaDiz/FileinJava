f2b7c8aad5b4c998060f957ede0c3b8c9915ed7d
Lisitha Dissanayaka <114801465+LisithaDiz@users.noreply.github.com>
2023-01-05 15:21:28 +0530

    Update Math.java

--- Math.java
+++ Math.java
@@ -1,19 +1,13 @@
-import java.io.IOException;
 import java.util.Scanner;
-import  java.io.FileWriter;
 public class Math {
-      private double a,b,c,d,e,f,g,h;
-      public double avgM(double quiz1, double quiz2, double quiz3, double quiz4, double quiz5, double test1, double test2, double finalExam){
+      private double  quiz1, quiz2, quiz3, quiz4, quiz5, test1, test2, finalExam ;
+      public double avgM(){
             double quiz=(quiz1+quiz2+quiz3+quiz4+quiz5)/500*15;
             double test_1=test1/100*30;
             double test_2=test2/100*20;
             double final_E=finalExam/100*35;
             double avg=quiz+test_1+test_2+final_E;
             return avg;
-      }
-      public  String name(String firstName, String lastName){
-            String fullName=firstName+" "+ lastName.replace(',',' ');
-            return fullName;
       }
       public String grade(double avg){
             String gradeABC;
@@ -30,21 +24,29 @@ public class Math {
             }
             return gradeABC;
       }
-      public void mathFinal() throws IOException {
+
+
+
+
+
+      public double getFinalEx(){
+            return this.finalExam;
+      }
+      public  String name(String firstName, String lastName){
+            String fullName=firstName+" "+ lastName.replace(',',' ');
+            return fullName;
+      }
+      public void mathMarks() {
             Scanner in = new Scanner(System.in);
-            a=in.nextDouble();
-            b=in.nextDouble();
-            c=in.nextDouble();
-            d=in.nextDouble();
-            e=in.nextDouble();
-            f=in.nextDouble();
-            g=in.nextDouble();
-            h=in.nextDouble();
-            Math objM=new Math();
-            double avgMM=objM.avgM(a,b,c,d,e,f,g,h);
-            String gradeMM =objM.grade(avgMM);
-            FileWriter myFile = new FileWriter("GradeAverage.txt");
-            String line=h+avgMM+gradeMM;
-            myFile.write(line);
+            System.out.println("Enter marks ");
+            quiz1=in.nextDouble();
+            quiz2=in.nextDouble();
+            quiz3=in.nextDouble();
+            quiz4=in.nextDouble();
+            quiz5=in.nextDouble();
+            test1=in.nextDouble();
+            test2=in.nextDouble();
+            finalExam=in.nextDouble();
+
       }
 }
